d6872e05c36f936ce37c361c6393d81dda253ea9
kumargoud394856 <104122056+kumargoud394856@users.noreply.github.com>
2022-09-07 15:52:31 +0530

    Update SpringBootSecurityUsingOrmAuthApplicationTests.java

--- SpringBootSecurityUsingORMAuth/src/test/java/com/example/demo/SpringBootSecurityUsingOrmAuthApplicationTests.java
+++ SpringBootSecurityUsingORMAuth/src/test/java/com/example/demo/SpringBootSecurityUsingOrmAuthApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootSecurityUsingOrmAuthApplicationTests {
 
-	@Test
+	
 	//hii iam kumargoud
+	//hello rajesh
 	void contextLoads() {
 	}
 
